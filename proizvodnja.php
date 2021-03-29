d5b8c4c793e8244e7ce77fab1957c36ed3b158ea
Zile028 <zile028@gmail.com>
2021-03-29 13:19:41 +0200

    proizvodnja page html and css

---
+++ proizvodnja.php
@@ -0,0 +1,21 @@
+<?php include 'partials/navigation.php' ?>
+<?php include 'partials/otherHeader.php' ?>
+<!-- proizvodnja -->
+<section class="proizvodnja">
+    <article>
+        <img src="img/poruci1.jpg" alt="">
+    </article>
+
+    <article>
+        <img src="img/poruci2.jpg" alt="">
+    </article>
+
+    <article>
+        <h2>Poruci sveze</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>Ipsa, rerum?</p>
+        <button>poruci</button>
+    </article>
+
+</section>
+
+<?php include 'partials/footer.php' ?>
