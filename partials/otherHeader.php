dfcd3d9d68ad83357850af46ed8cf7e00b8290a0
Zile028 <zile028@gmail.com>
2021-03-29 13:04:00 +0200

    add to partials header for other page, plan page html and css

---
+++ partials/otherHeader.php
@@ -0,0 +1,9 @@
+<!-- header -->
+<header class="otherHeader">
+    <article class="container">
+        <hgroup>
+            <h1>Odaberite zdravlje</h1>
+            <button>procitaj vise</button>
+        </hgroup>
+    </article>
+</header>
---
+++ plan.php
@@ -0,0 +1,39 @@
+<?php include 'partials/navigation.php' ?>
+<?php include 'partials/otherHeader.php' ?>
+<!-- green food -->
+<section class="green">
+    <article class="container">
+        <div>
+            <img src="img/zeleno1.jpg" alt="">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, architecto pariatur atque id ducimus
+                laudantium commodi corrupti consequatur debitis delectus, voluptas assumenda fugiat necessitatibus
+                temporibus. Nulla quos beatae, doloribus, blanditiis nam dolorem, obcaecati perspiciatis dignissimos et
+                corrupti laboriosam ipsa distinctio.</p>
+        </div>
+        <div>
+            <h2>Jedite zeleno</h2>
+            <img src="img/zeleno2.jpg" alt="">
+        </div>
+    </article>
+
+</section>
+
+<!-- organic food -->
+<section class="organic">
+    <article class="container">
+        <div>
+            <h2>Kupujte organsko</h2>
+            <img src="img/orgainc1.jpg" alt="">
+        </div>
+        <div>
+            <img src="img/organic3.jpg" alt="">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, architecto pariatur atque id ducimus
+                laudantium commodi corrupti consequatur debitis delectus, voluptas assumenda fugiat necessitatibus
+                temporibus. Nulla quos beatae, doloribus, blanditiis nam dolorem, obcaecati perspiciatis dignissimos et
+                corrupti laboriosam ipsa distinctio.</p>
+        </div>
+    </article>
+
+</section>
+
+<?php include 'partials/footer.php' ?>
