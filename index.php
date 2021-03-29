8566e68335db6aba3526f20cce148db27f04ad0c
Zile028 <zile028@gmail.com>
2021-03-29 12:23:39 +0200

    index page html and css

--- index.php
+++ index.php
@@ -10,5 +10,24 @@
         </hgroup>
     </article>
 </header>
-
+<!-- about -->
+<section class="about container">
+    <article>
+        <h2>Koraci do <span>zdravlja</span></h2>
+        <h3>01</h3>
+        <h4>Dole ugljeni hidrati</h4>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum possimus reprehenderit reiciendis distinctio a
+            est odit animi, enim aliquam quos earum maiores tempora architecto commodi vitae esse blanditiis illo.
+            Cupiditate!</p>
+    </article>
+    <article>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae iusto voluptates, repudiandae voluptas
+            enim eligendi magni accusantium delectus asperiores dolores.</p>
+        <h3>02</h3>
+        <h4>Gore proteini</h4>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum possimus reprehenderit reiciendis distinctio a
+            est odit animi, enim aliquam quos earum maiores tempora architecto commodi vitae esse blanditiis illo.
+            Cupiditate!</p>
+    </article>
+</section>
 <?php include 'partials/footer.php' ?>
